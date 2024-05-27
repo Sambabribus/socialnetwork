23486ec2b039d2e057893aba62098f54d0c20526
Sabrina <sabrina.talbi@viacesi.fr>
2024-05-27 14:50:41 +0200

    Simple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,30 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+
+  $sql = "SELECT id FROM user WHERE nickname = :username AND password = :password";
+
+
+  $stmt = $PDO->prepare($sql);
+
+
+  $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+  $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+
+
+  $stmt->execute();
+
+
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+  if ($result) {
+    return $result['id'];
+  } else {
+    return -1;
+  }
+}
